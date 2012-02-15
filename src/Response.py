4fe901e4a4ae8274cb61b4c33a4c584df5958041
bjornlevi <bjorn@gmail.com>
2012-02-15 16:44:37 -0500

    Added some basic routes

--- src/Response.py
+++ src/Response.py
@@ -15,13 +15,13 @@ def session():
     else:
         return None
 
+@bottle.get('/register')
+def register():
+    return 'registration page'
+
 @bottle.get('/login')
 def login_form():
-    return '''<form method="POST">
-                <input name="name" type="text" />
-                <input name="password" type="password" />
-                <input type=submit>
-              </form>'''
+    return 'login form'
 
 @bottle.post('/login')
 def login_submit():
@@ -29,6 +29,61 @@ def login_submit():
     password = bottle.request.forms.get('password')
     bottle.response.set_cookie('sessionid', name, 'asdf')
     return "<p>"+name + ':'+ password +"</p>"
+
+@bottle.get('/groups')
+def get_groups():
+    return 'list of groups'
+
+@bottle.post('/groups')
+def add_group():
+    return 'create new group'
+
+@bottle.post('/groups/:group')
+def edit_group():
+    return 'edit group'
+
+@bottle.post('/user')
+def add_user():
+    return 'register new user if user name does not exist'
+
+@bottle.get('/user/:user')
+def get_user():
+    return 'user page'
+
+@bottle.post('/user/:user')
+def edit_user():
+    return 'edit user'
+
+@bottle.get('/posts')
+def get_posts():
+    return 'posts list'
+
+@bottle.get('/posts/:post')
+def get_post():
+    return 'one post'
+
+@bottle.post('/posts')
+def add_post():
+    return 'new post'
+
+@bottle.post('/posts/:post')
+def edit_post():
+    return 'edit post'
+
+@bottle.post('/posts/:post/comment')
+def add_comment():
+    return 'new comment'
+
+@bottle.post('/posts/:post/comment/:commment')
+def edit_comment():
+    return 'edit comment'
+
+@bottle.post('/posts/:post/like')
+def add_like():
+    """
+    This method is activated by an ajax call and contains the type of like to add
+    """
+    return 'new like'
 
 @bottle.route('/')
 def hello():
