e2780d203718d3aca7e4d180890b457229eb35e8
DIAOSHUO <sdiao@brandeis.edu>
2012-03-01 11:50:11 -0500

    add queries.py tables.py

---
+++ src/Tables.py
@@ -0,0 +1,11 @@
+# coding=utf-8
+from Settings import Settings
+import sqlite3
+import bottle
+
+
+get_all_posts = """select * from posts"""
+get_all_user_posts = """select * from posts where user = ?"""
+#cursor.execute(get_all_user_posts, ('bjorn'))
+get_all_user_posts_after = """select * from posts where user = ? and date > ?"""
+#cursor.execute(get_all_user_posts_after, ('bjorn','01-01-2012 00:00:00'))
---
+++ src/queries.py
@@ -0,0 +1,39 @@
+# coding=utf-8
+#This file defines the schema of tables
+
+#######################LOCAL TABLE####################################3
+#posttable: (id, date, title, post, user, status)
+create_post_table = """"CREATE TABLE posts (id integer primary key, date text, title text, post text, user text, status text)"""
+
+#Shuo, comment_table, in each group dataase
+create_comment_table = """ CREATE TABLE comments (id integer PRIMARY KEY, date text, content text, post text, user text)"""
+
+###########################  GLOBAL TABLE  #############################################333
+#Shuo, user_table, global #what information do we need to store, in addition to username and password?
+create_user_table = """ CREATE TABLE users (id integer PRIMARY KEY, user text, pwd text, email text )"""
+
+# ???why do we put image in user_settings_table?~instead of user_table
+create_user_settings_table = """CREATE TABLE user_settings (id integer primary key, user text, image text, group text, about text)"""
+
+# ???table groups other attribute? How do the system link to a single group's database? 
+create_group_table = """ CREATE TABLE groups (id integer PRIMARY KEY, group text, db text)"""
+
+create_access_table = """CREATE TABLE access (id integer primary key, user text, group text, role text)"""
+#for example user in group global with role 'admin' can create new groups
+#user with group 'internet and society' and role 'admin' can't create new groups
+#he can however accept group applications and assign roles for people in that group
+#group creator automatically becomes group admin as well
+
+##############################  USE FOR Login/Logout   ####################
+create_session_table = """CREATE TABLE sessions (id integer primary key, user text, sessionid text, date text)"""
+
+
+################################# DROP TABLES, just for test  ###########################
+drop_user_table =""" DROP TABLE users"""
+drop_user_setting_table = """"DROP TABLE user_settings"""
+drop_group_table ="""" DROP TABLE groups"""
+drop_access_table = """"DROP TABLE access"""
+drop_post_table = """DROP TABLE posts"""
+drop_comment_table = """DROP TABLE comments"""
+
+################################INSERT INTO TABLES
